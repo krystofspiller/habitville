1053f57e79b3b26e23bfaf75c88b72122398a07c
Krystof Spiller <krystof.spiller@carta.com>
2024-09-04 01:14:18 +0200

    use vitest instead of jest

--- src/util/level.ts
+++ src/util/level.ts
@@ -3,27 +3,27 @@ import { type Range } from './types'
 type DefinedLevel = Range<1, 22>
 
 const xpNeededForLevel: Record<DefinedLevel, number> = {
-  1: 0,
-  2: 10,
-  3: 15,
-  4: 20,
-  5: 30,
-  6: 50,
-  7: 75,
-  8: 100,
-  9: 150,
-  10: 250,
-  11: 500,
-  12: 800,
-  13: 1250,
-  14: 1900,
-  15: 2900,
-  16: 4400,
-  17: 6500,
-  18: 10000,
-  19: 15000,
-  20: 22000,
-  21: 33000,
+  1: 5, // 5
+  2: 10, // 15
+  3: 15, // 30
+  4: 22, // 52
+  5: 33, // 85
+  6: 50, // 135
+  7: 75, // 210
+  8: 110, // 320
+  9: 160, // 480
+  10: 250, // 730
+  11: 380, // 1110
+  12: 590, // 1700
+  13: 900, // 2600
+  14: 1400, // 4000
+  15: 2100, // 6100
+  16: 3100, // 9200
+  17: 4600, // 13800
+  18: 7000, // 20800
+  19: 10500, // 31300
+  20: 16000, // 47300
+  21: 24000, // 71300
 }
 
 const totalXpNeededForLevel = Object.keys(xpNeededForLevel).reduce(
@@ -49,15 +49,17 @@ export function getLevel(xp: number) {
     throw new Error('Level not found')
   }
 
-  const xpNeededForNextLevel = xpNeededForLevel[(level + 1) as DefinedLevel]
+  const xpNeededForNextLevel = xpNeededForLevel[level]
   const currentLevelXpProgressAbs =
-    xp - totalXpNeededForLevel[(level - 1) as DefinedLevel]
+    xp - (level > 1 ? totalXpNeededForLevel[(level - 1) as DefinedLevel] : 0)
 
   return {
     level,
     xpNeededForNextLevel,
     currentLevelXpProgressAbs,
-    currentLevelXpProgressPct:
+    currentLevelXpProgressPct: Math.min(
       (currentLevelXpProgressAbs / xpNeededForNextLevel) * 100,
+      100,
+    ),
   }
 }
