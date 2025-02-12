c7ced58c691ba23b5a987dd515bc3a05f8650470
Laegas <krystof.spiller@gmail.com>
2025-02-12 10:11:43 +0100

    cleanup, basic assets and tailwind setup

--- apps/habitville-expo/app/_layout.tsx
+++ apps/habitville-expo/app/_layout.tsx
@@ -1,8 +1,33 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router"
+import { useFonts } from 'expo-font'
 
-import "../global.css";
+import "../global.css"
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+  
   return (
     <Stack>
       <Stack.Screen name="index" options={{headerShown: false}} />
